45faa5d92837679f9cf8c992c82b08d9e805ffdc
ChihChia Wang <cwang@cs.nctu.edu.tw>
2022-01-10 05:50:35 +0800

    chore(Terraform): 🔧 更新 provider proxmox 設定值註解

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,14 @@
 provider "proxmox" {
-  pm_api_url      = "https://192.168.113.240:8006/api2/json"
   pm_user         = var.PM_USERNAME
   pm_password     = var.PM_PASSWORD
   pm_tls_insecure = true
 
+  #FOR PROD
+  pm_api_url = "https://192.168.113.240:8006/api2/json"
+
   # #FOR DEVELOP
-  # pm_api_url      = "https://${module.pm_tunnel.host}:${module.pm_tunnel.port}/api2/json"
-  # pm_debug = true
+  # pm_api_url    = "https://${module.pm_tunnel.host}:${module.pm_tunnel.port}/api2/json"
+  # pm_debug      = true
   # pm_log_enable = true
   # pm_log_file   = "terraform-plugin-proxmox.log"
   # pm_log_levels = {
